5e2fc13e0d3fc5c7fcef38adba93c57f617a2e78
Kevin Pugliese <63055465+kevpug@users.noreply.github.com>
2024-07-11 04:53:04 -0400

    rebuild

--- src/context.tsx
+++ src/context.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import type { UnistylesThemes } from "./global";
 import { unistyles } from "./core";
+import type { UnistylesTheme } from "./types";
 
-export const ThemeContext = React.createContext({} as UnistylesThemes[keyof UnistylesThemes]);
+export const ThemeContext = React.createContext({} as UnistylesTheme);
 
 export const UnistylesProvider = ({ theme, children }: { theme: keyof UnistylesThemes; children: React.ReactNode }) => (
     <ThemeContext.Provider value={unistyles.registry.getTheme(theme)}>{children}</ThemeContext.Provider>
